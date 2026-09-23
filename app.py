09976193d8b8ac0a755088e0426ded20f7f36320
brandonmtruong-byte <brandonmtruong@gmail.com>
2026-09-24 00:13:36 +1000

    dpi and width

--- app.py
+++ app.py
@@ -795,4 +795,4 @@ elif st.session_state.active_view == 'Window Diagram Creator':
 
     st.markdown("---")
     render_eyebrow("Diagram")
-    st.image(diagram_png)
+    st.image(diagram_png, width=500)
